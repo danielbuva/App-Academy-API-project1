7fda31222679e9e7b496357f2405c861dac589a3
dani <daniel.valdecantos@gmail.com>
2023-05-10 13:48:12 -0700

    feat: add user authentication middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,20 @@
+const {
+  setTokenCookie,
+  restoreCsrf,
+  restoreSession,
+  verifyAuth,
+} = require("../../services/auth.server");
 const router = require("express").Router();
 
-router.get("/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    "XSRF-Token": csrfToken,
-  });
-});
+const returnUser = (req, res) => {
+  return res.json(req.user);
+};
+
+router.use(restoreSession);
+
+router.get("/csrf/restore", restoreCsrf);
+router.get("/set-token-cookie", setTokenCookie);
+router.get("/require-auth", verifyAuth, returnUser);
 
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
---
+++ backend/services/auth.server.js
@@ -0,0 +1,85 @@
+const jwt = require("jsonwebtoken");
+const { jwtConfig, isProduction } = require("../config");
+const { User } = require("../db/models");
+const { secret, expiresIn } = jwtConfig;
+
+const setTokenCookie = async (_, res) => {
+  const data = await User.findOne({
+    where: {
+      username: "Demo-lition",
+    },
+  });
+
+  const user = {
+    id: data.id,
+    email: data.email,
+    username: data.username,
+  };
+
+  const token = jwt.sign(
+    { data: user },
+    secret,
+    { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
+  );
+
+  res.cookie("token", token, {
+    maxAge: expiresIn * 1000, // in milliseconds
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: "lax",
+  });
+
+  return res.json({ user });
+};
+
+const restoreSession = (req, res, next) => {
+  const { token } = req.cookies;
+  req.user = null;
+
+  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+    if (err) {
+      return next();
+    }
+
+    try {
+      const { id } = jwtPayload.data;
+      req.user = await User.findByPk(id, {
+        attributes: {
+          include: ["email", "createdAt", "updatedAt"],
+        },
+      });
+    } catch (e) {
+      res.clearCookie("token");
+      return next();
+    }
+
+    if (!req.user) res.clearCookie("token");
+
+    return next();
+  });
+};
+
+const verifyAuth = (req, _res, next) => {
+  if (req.user) return next();
+
+  const err = new Error("Authentication required");
+  err.title = "Authentication required";
+  err.errors = { message: "Authentication required" };
+  err.status = 401;
+  return next(err);
+};
+
+const restoreCsrf = (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
+  });
+};
+
+module.exports = {
+  setTokenCookie,
+  restoreSession,
+  verifyAuth,
+  restoreCsrf,
+};
