de4ff42c4ea7931126d34298c71e311837bb1b81
dani <daniel.valdecantos@gmail.com>
2023-05-10 14:48:43 -0700

    feat: add signup endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,27 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const {
+  setTokenCookie,
+  requireAuth,
+} = require("../../services/auth.server");
+const { User } = require("../../db/models");
+
+router.post("/", async (req, res) => {
+  const { email, password, username } = req.body;
+  const hashedPassword = bcrypt.hashSync(password);
+  const user = await User.create({ email, username, hashedPassword });
+
+  const safeUser = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+  };
+
+  setTokenCookie(res, safeUser);
+
+  return res.json({
+    user: safeUser,
+  });
+});
 
 module.exports = router;
